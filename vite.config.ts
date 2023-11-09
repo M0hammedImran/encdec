571c896631dc3d2b33ecc0eacf8adf312fed40ba
Mohammed Imran <imran@factoreal.com>
2023-11-09 16:49:07 +0530

    Add encryption and decryption functionality to library

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,4 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-	build: {
-		lib: {
-			entry: "./src/encdec.ts",
-			name: "encdec",
-			fileName: "encdec",
-			formats: ["es", "umd", "cjs"],
-		},
-	},
-});
+export default defineConfig({});
 
